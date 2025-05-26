7f7731ac16a8591d54551083cfd29d3aafe58603
RakhaNoor01 <05530@smkrus.sch.id>
2025-05-26 08:39:12 +0700

    first commit

---
+++ app/src/main/java/com/example/pas_absen06_absen32/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.pas_absen06_absen32.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
