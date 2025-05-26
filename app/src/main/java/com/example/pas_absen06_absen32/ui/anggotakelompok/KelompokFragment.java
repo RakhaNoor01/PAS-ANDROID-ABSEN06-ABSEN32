bf92093f718a50e7f76c327a2098966ef8995f3e
Azelzy <azkaelfachrizy@gmail.com>
2025-05-26 09:56:01 +0700

    lupa nambahin

--- app/src/main/java/com/example/pas_absen06_absen32/ui/anggotakelompok/KelompokFragment.java
+++ app/src/main/java/com/example/pas_absen06_absen32/ui/anggotakelompok/KelompokFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.pas_absen06_absen32.R;
 import com.example.pas_absen06_absen32.adapter.MemberAdapter;
 import com.example.pas_absen06_absen32.model.Member;
 
@@ -20,7 +21,7 @@ public class KelompokFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View view = inflater.inflate(R.layout.fragment_kelompok, container, false);
 
         recyclerView = view.findViewById(R.id.rvMembers);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
